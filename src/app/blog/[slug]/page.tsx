974c443daa516e55b26fd6b0227b1abf9d66f4c0
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-02-07 22:39:26 +0500

    get single post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { BlogsService } from "@/services/blogs.service";
 
-const SingleBlog = () => {
-  return <div>SingleBlog</div>;
+async function getData(id: string) {
+  try {
+    const detailedBlog = await BlogsService.getDetailedBlog(id);
+    return detailedBlog;
+  } catch (error) {
+    console.error("Error fetching detailed blog:", error);
+    return null;
+  }
+}
+
+const BlogDetailedPage = async ({ params }: { params: { slug: string } }) => {
+  const data = await getData(params.slug);
+  console.log(data);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="mt-20">
+      <span>{data.title}</span>
+      <div dangerouslySetInnerHTML={{ __html: `${data?.content?.html}` }}></div>
+    </div>
+  );
 };
 
-export default SingleBlog;
+export default BlogDetailedPage;
