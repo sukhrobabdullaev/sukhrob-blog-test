7b281d5b7aaeb168276e52a1e3c8a180170e965d
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-02-08 19:53:21 +0500

    indicator

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -35,51 +35,6 @@ const BlogPage = async () => {
         ) : (
           <h3>No posts available.</h3>
         )}
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, pariatur.
-        Soluta earum, iure reiciendis incidunt id perferendis explicabo culpa
-        tempore dolorem officiis amet odit, nemo quasi aspernatur sapiente
-        repellat, quae obcaecati commodi praesentium. Ad temporibus eum fugiat
-        voluptates inventore ducimus quisquam veniam in explicabo recusandae
-        voluptatum aspernatur blanditiis placeat ut delectus dolores doloremque
-        voluptas, consequatur necessitatibus sunt expedita nulla cumque. At
-        fugit recusandae, quo cum rerum mollitia temporibus dolore amet deserunt
-        ex odit illo provident consequuntur, inventore maiores ducimus, est
-        eligendi exercitationem ut ea cupiditate itaque libero? Dolores quia
-        quisquam dolore, necessitatibus alias nisi amet illo dicta aliquam
-        obcaecati ut odio, eius numquam consequuntur temporibus libero quae est
-        ad expedita accusantium ducimus suscipit esse laboriosam. Repudiandae,
-        dolorum saepe, reprehenderit quas neque nemo modi incidunt veniam
-        consectetur deserunt ducimus quaerat. Tempore sapiente cupiditate
-        voluptate. Quis consequuntur voluptatem qui laudantium obcaecati,
-        architecto illo quam labore voluptatum officia quae possimus ratione,
-        quod aspernatur eius commodi harum nulla, rem veritatis ea. Tempore in
-        labore accusamus. Illo ab quis saepe optio quae quas? Architecto dolore
-        at ad eligendi odit? Omnis commodi dolor fugit enim eius ab aliquam
-        obcaecati incidunt voluptatibus reprehenderit exercitationem
-        repellendus, quas, a sapiente quaerat dolores illum alias minima
-        eligendi harum, molestiae perspiciatis! Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Voluptatibus quam voluptatum inventore
-        incidunt officiis? Quisquam qui quaerat hic laudantium temporibus odio,
-        deserunt vitae reprehenderit, nobis aut, quasi excepturi sint ut dolore
-        eius ad unde libero. Rerum voluptas iste, similique et ullam impedit?
-        Rem laudantium laborum quasi praesentium obcaecati vero optio nam
-        corporis sint, necessitatibus molestias magni debitis iusto, saepe,
-        repudiandae impedit! Molestias dolorum adipisci reprehenderit, fuga
-        quasi illum fugit aut earum quibusdam expedita pariatur, omnis culpa
-        minus perferendis possimus. Est possimus quo inventore cum doloremque
-        nihil aliquid cupiditate hic. Perferendis omnis culpa harum repellendus,
-        repudiandae cumque. Quae, aspernatur aliquid possimus fugiat, earum
-        exercitationem molestiae perferendis, voluptates amet voluptatibus
-        tempora excepturi veritatis nam. Voluptate eius possimus ipsum tempora
-        obcaecati, veritatis illum. Vero magnam praesentium qui quidem minima
-        maxime inventore ipsa aspernatur temporibus incidunt facilis tempore
-        illum aliquam voluptatibus culpa odit minus atque dolorem velit nam
-        possimus consequatur, est ad! Ducimus iure eius doloribus totam
-        dignissimos itaque hic obcaecati, officia id in beatae non quo, enim
-        reiciendis illum perferendis, nam laboriosam aperiam iste facilis
-        ratione? Natus quis vitae vero molestias? Similique ex sit minima esse
-        vero, aliquam pariatur non omnis earum tenetur voluptates unde, dolores
-        nisi illo suscipit culpa. Hic, cupiditate porro?
       </div>
       <div className="flex flex-col gap-4 md:sticky md:top-16 md:w-80 md:h-80 p-4">
         <h3 className="text-center  font-bold md:text-3xl  text-2xl">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/navbar";
 import Footer from "@/components/shared/footer";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import NextTransitionBar from "next-transition-bar";
 
 const montserrat = Montserrat({
   weight: ["300", "400", "500"],
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Sukhrob's blog",
-  description: "A software engineer, computer science enthusiast",
-};
+// export const metadata: Metadata = {
+//   title: "Sukhrob's blog",
+//   description: "A software engineer, computer science enthusiast",
+// };
 
 export default function RootLayout({
   children,
@@ -29,6 +32,13 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NextTransitionBar
+            color="#03fcdf"
+            height={2}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #29d, 0 0 5px #29d"
+          />
           <Navbar />
           <main>{children}</main>
           <Footer />
