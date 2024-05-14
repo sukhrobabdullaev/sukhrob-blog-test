2d79c3aff588aac6e31b844aabc7e487d889ae74
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-05-14 23:20:44 +0500

    updted

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col gap-2 justify-center items-center h-screen">
-      <h2 className="text-3xl font-bold">Not Found</h2>
-      <p>Could not find requested resource</p>
+      <h2 className="text-3xl font-bold">Topilmadi 😌</h2>
+      <p>So'ralgan manba topilmadi</p>
       <Link href="/" className="text-cyan-500 text-lg underline">
-        Return Home
+        Asosiy sahifaga qaytish
       </Link>
     </div>
   );
